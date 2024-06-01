2031d1fcbb1e72d60cc305eae98d5d20e274d0e1
твое имя < >
2024-06-01 20:40:14 +0400

    hw-2

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+// Написать обработчик запроса:
+// - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
+// - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
+// - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
+// - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
+// - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+
+
+const getUsers = require('./modules/users');
+
+const hostname = '127.0.0.1';
+const port = 3003;
+
+const server = http.createServer((request, response) => {
+    const ipAddress = "http://127.0.0.1";
+    const url = new URL(request.url, ipAddress);
+    const userName = url.searchParams.get('hello');
+
+    if (userName) {
+        response.status = 200;
+        response.statusMessage = "OK"
+        response.header = "Content-Type: text/plain"
+        response.write(`Hello, ${userName}.`);
+        response.end();
+        return;
+    }
+    if (request.url === '/?hello') {
+        response.status = 400;
+        response.statusMessage = "OK"
+        response.header = "Content-Type: text/plain"
+        response.write('Enter a name');
+        response.end();
+        return;
+    }
+    if (request.url === '/users') {
+        response.status = 200;
+        response.statusMessage = "OK"
+        response.header = "Content-Type: application/json"
+        response.write(getUsers());
+        response.end();
+        return;
+    }
+    if (request.url === "/") {
+        response.status = 200;
+        response.statusMessage = "OK"
+        response.header = "Content-Type: text/plain"
+        response.write("Hello, word!");
+        response.end()
+    }
+
+    response.status = 500;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: text/plain";
+    response.write("");
+    response.end();
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Сервер запущен по адресу http://${hostname}:${port}/`)
+});
